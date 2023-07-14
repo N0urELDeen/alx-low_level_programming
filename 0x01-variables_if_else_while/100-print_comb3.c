2400a2a0f200c120db33a04c962013c6087abd82
N0urELDeen <5noureldeen5@gmail.com>
2023-07-14 15:43:58 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,22 @@
  */
 int main(void)
 {
-        int d, a;
+	int d, a;
 
-        for (d = 0; d <= 8; d++)
-        {
-                for (a = d + 1; a <= 9; a++)
-                {
-                        putchar(d + '0');
-                        putchar(a + '0');
+	for (d = 0; d <= 9; d++)
+	{
+		for (a = 0; a <= 9; a++)
+		{
+			putchar(d + '0');
+			putchar(a + '0');
 
-                        if (d != 8 || a != 9)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
-        putchar('\n');
-
-        return (0);
+			if (d != 8 || a != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
