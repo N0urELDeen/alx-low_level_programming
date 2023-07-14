0b0c2aaf678731b22980c7c6c9012f89a787e298
N0urELDeen <5noureldeen5@gmail.com>
2023-07-14 16:24:29 -0700

    update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Alwys 0 (Success)
+ */
+
+int main(void)
+{
+	int d, a, c;
+
+	for (d = 0; d <= 9; d++)
+	{
+		for (a = d + 1; a <= 8; a++)
+		{
+			for (c = d + a + 1; c <= 7; c++)
+			{
+				putchar(d + '0');
+				putchar(a + '0');
+				putchar(c + '0');
+
+				if (d != 7 || a != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
