595194174c224bbaee6cd2b44da7acda6ef650b3
N0urELDeen <5noureldeen5@gmail.com>
2023-07-14 11:15:20 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	putchar(d + '0');
+
+	putchar('\n');
+
+	return (0);
+}
