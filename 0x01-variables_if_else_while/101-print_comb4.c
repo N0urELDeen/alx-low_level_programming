63e72c935a1106d07e195d30bc9035865b72ca95
N0urELDeen <5noureldeen5@gmail.com>
2023-07-14 16:30:42 -0700

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int d, a, c;
 
-	for (d = 0; d <= 9; d++)
+	for (d = 0; d <= 7; d++)
 	{
 		for (a = d + 1; a <= 8; a++)
 		{
-			for (c = d + a + 1; c <= 7; c++)
+			for (c = a + 1; c <= 9; c++)
 			{
 				putchar(d + '0');
 				putchar(a + '0');
