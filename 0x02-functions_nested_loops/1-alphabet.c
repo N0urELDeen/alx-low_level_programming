765a3857fe66875e35f958823d98ee1fdf896f59
N0urELDeen <5noureldeen5@gmail.com>
2023-07-18 11:18:00 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,11 @@ void print_alphabet(void)
 {
 	char c;
 
-	for (c ='a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c);
+		_putchar(c);
 	}
-	putchar('\n')
+	_putchar('\n')
 
 	return (0);
 }
