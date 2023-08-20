17d786500b09037ebcab54a72808594ef75c5510
N0urELDeen <5noureldeen5@gmail.com>
2023-08-21 01:55:15 +0300

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _callpc - allocate memory using malloc and initialize it to zero
- * @nmemb: number of elements
- * @size: size of the memory block to be allocated 
- *
- * Retuen: poiner to the adrres of the memory block
- */
-
+* _calloc - allocate memory using malloc and initialize it to zero
+*@nmemb: number of elements
+*@size: size of the memory block to be allocated
+*
+*Return: poiner to the address of the memory block
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *block;
 	unsigned int i;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	block = malloc(nmemb * size);
@@ -20,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		for (i = 0; i < (nmemb * size); i++)
 			block[i] = 0;
-		retuen (block);
+		return (block);
 	}
 	else
 		return (NULL);
